4b121099abb40d82134e636eaed8a5dcae24ab93
jasonchee816 <jasonchee01@hotmail.com>
2023-02-24 00:01:27 +0800

    Add Migration and Seeder

---
+++ database/seeders/dbSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class dbSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Users
+
+
+        //Wallets
+
+
+        //Transactions
+
+
+        //TransactionCatergories
+
+
+    }
+}
