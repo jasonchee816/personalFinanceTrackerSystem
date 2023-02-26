991572316e8067033c37e5c2c11ed57fbac11d86
jasonchee816 <jasonchee01@hotmail.com>
2023-02-27 01:41:33 +0800

    Create Table and Seeder might require changes

--- database/migrations/2023_02_23_155513_create_tables.php
+++ database/migrations/2023_02_23_155513_create_tables.php
@@ -13,19 +13,18 @@ class CreateTables extends Migration
      */
     public function up()
     {
-        Schema::create('tables', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-
         //Users
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('email');
+            $table->string('password');
+            $table->bigInteger('tel_no');
+            $table->string('name');
+            $table->boolean('is_admin');
+        });
 
 
         //Wallets
-
-
-        //Transactions
-
 
         //TransactionCatergories
         Schema::create('TransactionCategories', function (Blueprint $table) {
@@ -33,6 +32,21 @@ class CreateTables extends Migration
             $table->string('name');
             $table->type();
             $table->timestamps();
+        });
+
+        //Transactions
+        Schema::create('Transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->double('amount');
+            $table->string('description');
+            $table->integer('walletId');
+            // $table->foreign('walletId')->references('id')->on('Wallets');
+            $table->integer('category');
+            $table->foreign('category')->references('id')->on('TransactionCategories');
+            $table->timestamp('transDate');
+
+            // $table->boolean('type');
+            //$table->enum('method', ['income', 'expense']); 
         });
 
 
@@ -45,6 +59,9 @@ class CreateTables extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tables');
+        Schema::dropIfExists('Users');
+        Schema::dropIfExists('Wallets');
+        Schema::dropIfExists('TransactionCategories');
+        Schema::dropIfExists('Transactions');
     }
 }
--- database/seeders/dbSeeder.php
+++ database/seeders/dbSeeder.php
@@ -14,12 +14,18 @@ class dbSeeder extends Seeder
     public function run()
     {
         //Users
-
+        for($i=1;$i<10;$i++){
+            DB::table('users')->insert([
+                'email'=>Str::random(10).'@outlook.com',
+                'password'=>Hash::make('password'),
+                'tel_no'=>random_int(1000000000, 9999999999),
+                'name'=>Str::random(10),
+                'is_admin'=>(bool)random_int(0, 1)
+            ]);
+        }
 
         //Wallets
 
-
-        //Transactions
 
 
         //TransactionCatergories 0 = Expenses 1 = Income
@@ -70,6 +76,21 @@ class dbSeeder extends Seeder
                 'type' => 1,
                 )
                 ));
+        }
+
+        //Transactions
+        for($i=0; $i<10; $i++){
+            DB::table('transcations')->insert([
+                'amount' => rand(0, 10000) / 100,
+                'description'=>Str::random(10),
+                // 'walletId'=>rand(0,10),
+                //look into factories? else will very hideous
+                //TO_DO
+                    //fake()->name(),
+                    //Migration create table change
+                    //Models making + factories?
+                    //separate seeders too 
+            ]);
         }
 
     }
