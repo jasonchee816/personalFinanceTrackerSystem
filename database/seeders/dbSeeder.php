7c7bff4c702beb22bce4f3591d0f5e299993de5d
jasonchee816 <jasonchee01@hotmail.com>
2023-03-05 18:30:46 +0800

    Seeder added Constant Test Data

--- database/seeders/dbSeeder.php
+++ database/seeders/dbSeeder.php
@@ -24,17 +24,49 @@ class dbSeeder extends Seeder
 
 
         //Users
+        //normal user 
+        DB::table('users')->insert([
+            'email'=>"jason@gmail.com",
+            'password'=>"1234",
+            'tel_no'=>"0123456789",
+            'name'=>"Jason",
+            'is_admin'=>0
+        ]);
+        //admin user
+        DB::table('users')->insert([
+            'email'=>"john@gmail.com",
+            'password'=>"1234",
+            'tel_no'=>"01987654321",
+            'name'=>"John",
+            'is_admin'=>1
+        ]);
+
+        //Random Data
         for($i=0;$i<5;$i++){
             DB::table('users')->insert([
                 'email'=>$faker->email,
                 'password'=>$faker->password,
                 'tel_no'=>$faker->phoneNumber,
                 'name'=>$faker->name,
-                'is_admin'=>(bool)random_int(0, 1)
+                'is_admin'=>0,
             ]);
         }
 
         //Wallets
+        //For test user  
+        for($i=0; $i<3; $i++){
+            DB::table('wallets')->insert([
+                'name'=>$faker->name,
+                'balance' => 1000,
+                'initial_balance' => 1000,
+                'type' => $faker->randomElement(
+                    Wallet::WALLET_TYPE,
+                ),
+                'user_id' => 1,
+            ]);
+        }
+
+        //Random Data 
         for($i=0; $i<10; $i++){
             DB::table('wallets')->insert([
                 'name'=>$faker->name,
@@ -43,13 +75,13 @@ class dbSeeder extends Seeder
                 'type' => $faker->randomElement(
                     Wallet::WALLET_TYPE,
                 ),
-                'user_id' => rand(1, 5)
+                'user_id' => rand(2, 5)
             ]);
         }
 
 
 
-        //TransactionCatergories 0 = Expenses 1 = Income
+        //Transaction_Catergories
         DB::table('Transaction_Categories')->insert(array(
             array(
             'name' => 'Food & Beverage',
@@ -98,11 +130,23 @@ class dbSeeder extends Seeder
         ));
 
         //Transactions
+        //for test user
+        for($i=0; $i<6; $i++){
+            DB::table('transactions')->insert([
+                'amount' => $faker->randomFloat(2, 10, 10000),
+                'description'=>Str::random(6),
+                'wallet_id'=>rand(1,3),
+                'category'=> rand(1, 11),
+                'trans_date'=>$faker->dateTime,
+            ]);
+        }
+
+        //random data
         for($i=0; $i<10; $i++){
             DB::table('transactions')->insert([
                 'amount' => $faker->randomFloat(2, 10, 10000),
-                'description'=>Str::random(10),
-                'wallet_id'=>rand(1,10),
+                'description'=>Str::random(6),
+                'wallet_id'=>rand(4,10),
                 'category'=> rand(1, 11),
                 'trans_date'=>$faker->dateTime,
             ]);
