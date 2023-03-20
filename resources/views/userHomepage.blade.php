3d8d2ed99ad3c75602f2a3112712de0cff8f1a9b
crackerskr <weiling42821@gmail.com>
2023-03-20 22:17:49 +0800

    user homepage

--- resources/views/userHomepage.blade.php
+++ resources/views/userHomepage.blade.php
@@ -4,14 +4,14 @@
 <div class="container1">
     <h1>Hi, {{}}</h1>
     <a href="createWallet" class="mb-4 btn">
-        <div class="card box-shadow p-2 walletBtn text-start" style="background-color: #1D7874;">
+        <div class="card box-shadow p-2 walletBtn text-start" style="background-color: #406E8E;">
             <div class="card-body">
                 <h5 class="card-title text-white">+ Add New Wallet</h5>
             </div>
         </div>
     </a>
     <a href="createTrans" class="mb-4 btn">
-        <div class="card box-shadow p-2 walletBtn text-start" style="background-color: #1D7874;">
+        <div class="card box-shadow p-2 walletBtn text-start" style="background-color: #406E8E;">
             <div class="card-body">
                 <h5 class="card-title text-white">+ Add New Transaction</h5>
             </div>
